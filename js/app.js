484faeb1ee540b9acde298987d1369700e6aea63
glaucoSapucaia <glaucopco@gmail.com>
2024-05-19 09:27:03 -0300

    Classe Despesa | Instanciando objetos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+// despesa object
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
 // index | Lançamento
 function cadastrarDespesa() {
     // recuperando valores
@@ -8,10 +20,10 @@ function cadastrarDespesa() {
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
 
-    console.log(ano.value)
-    console.log(mes.value)
-    console.log(dia.value)
-    console.log(tipo.value)
-    console.log(descricao.value)
-    console.log(valor.value)
+    // instanciando classe
+    let despesa = new Despesa(
+        ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value
+    )
+
+    console.log(despesa)
 }
