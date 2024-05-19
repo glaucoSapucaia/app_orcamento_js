b063b7a20b96452333b21efdabf1a089d5c78c2a
glaucoSapucaia <glaucopco@gmail.com>
2024-05-19 09:21:10 -0300

    Recuperando valores | document. | index

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// index | Lançamento
+function cadastrarDespesa() {
+    // recuperando valores
+    let ano = document.getElementById('ano')
+    let mes = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let tipo = document.getElementById('tipo')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+
+    console.log(ano.value)
+    console.log(mes.value)
+    console.log(dia.value)
+    console.log(tipo.value)
+    console.log(descricao.value)
+    console.log(valor.value)
+}
